3a4ab3c832325d7f24e181a1609537d471094eaf
Marek Hulan <mhulan@redhat.com>
2016-03-29 15:20:41 +0200

    First draft of runlist UI
    This version has persistence of JSON in CachedRunList model. It does not
    follow chef JSON format so the editor does not bring big value, just
    that I didn't have to code UI myself. We do the parsing so we keep
    chef-like schema in DB.
    
    Next step is to see if we could get the editor working with chef-like
    schema. And add orchestration.

---
+++ app/models/foreman_chef/cached_run_list.rb
@@ -0,0 +1,94 @@
+module ForemanChef
+  class CachedRunList < ActiveRecord::Base
+    serialize :list
+
+    belongs_to_host
+
+    # TODO add orchestration
+    # on save (gets triggered only when there's attr change) we should enforce the change on chef server
+    # we should check list.changed? just to be sure, it works even with serialized list
+    # opposite direction should work differently, on existing host form we should check chef-server version and
+    #   display warning if cache is out of date and have action to refresh it
+    #   or we could always load server version
+    # for new hosts that shouldn't matter at all
+    # we could also start updating cache based on incoming facts
+    # later - roles should become first class citizen and be synced together with environments
+
+    class Jail < Safemode::Jail
+      allow :to_chef_json, :as_chef_json
+    end
+
+    def self.parse(run_list_data, run_list_object = nil)
+      data = parse_data(run_list_data)
+
+      if run_list_object.nil?
+        cached_run_list = new(:list => data)
+      else
+        cached_run_list = run_list_object.clone
+        cached_run_list.list = data
+      end
+
+      cached_run_list
+    end
+
+    # return data in format ready for serialization
+    # which means Hash like this
+    # { :run_list => [ 'role[default]', 'recipe[foreman]' ] }
+    def self.parse_data(run_list_data)
+      case run_list_data
+        when Array
+          parse_array(run_list_data)
+        when Hash
+          run_list_data = run_list_data.with_indifferent_access
+          if run_list_data.has_key?(:run_list)
+            parse_array(run_list_data.with_indifferent_access[:run_list])
+          else
+            # from form we get {'0' => { 'name' => ...}, '1' => {...}}
+            parse_array(run_list_data.values)
+          end
+        else
+          raise ArgumentError, 'unsupported run_list_data format'
+      end
+    end
+    
+    def self.parse_array(run_list_array)
+      raise ArgumentError, "run_list is #{run_list_array.class}, expected Array" unless run_list_array.is_a?(Array)
+
+      if run_list_array.first.is_a?(Hash)
+        run_list_array.map { |item| item_to_chef(item) }
+      elsif run_list_array.first.is_a?(String)
+        run_list_array
+      else
+        raise ArgumentError, "run_list_array Array contains unknown data format of class #{run_list_array.class}"
+      end
+    end
+
+    # converts 'role[default]' to { :type => 'role', :name => 'default' }
+    def self.item_to_form(item)
+      type, name = item.split('[')
+      { :type => type, :name => name.chop! }
+    end
+
+    # converts { :type => 'role', :name => 'default' } to 'role[default]'
+    def self.item_to_chef(item)
+      item = item.with_indifferent_access
+      "#{item[:type]}[#{item[:name]}]"
+    end
+
+    def as_chef_json
+      { :run_list => self.list }
+    end
+
+    def to_chef_json
+      as_chef_json.to_json
+    end
+
+    def as_form_json
+      list.map { |item| self.class.item_to_form(item) }
+    end
+
+    def to_form_json
+      as_form_json.to_json
+    end
+  end
+end
--- app/models/foreman_chef/concerns/host_extensions.rb
+++ app/models/foreman_chef/concerns/host_extensions.rb
@@ -6,6 +6,21 @@ module ForemanChef
       included do
         alias_method_chain :set_hostgroup_defaults, :chef_attributes
         attr_accessible :chef_private_key
+
+        # even with autosave, save is called only if there's some change in attributes
+        has_one :cached_run_list, :autosave => true, :class_name => 'ForemanChef::CachedRunList', :foreign_key => :host_id
+        attr_accessible :run_list
+      end
+
+      def run_list
+        self.cached_run_list || ForemanChef::CachedRunList.parse(['role[default]'], self.build_cached_run_list)
+      end
+
+      def run_list=(run_list)
+        # returns CachedRunList instance, if there was one for host, return that one with modified attributes
+        @run_list = ForemanChef::CachedRunList.parse(run_list, self.cached_run_list)
+        # this ensures that host#save will save the cached run list too
+        self.cached_run_list = @run_list
       end
 
       def set_hostgroup_defaults_with_chef_attributes
@@ -15,4 +30,8 @@ module ForemanChef
       end
     end
   end
+end
+
+class ::Host::Managed::Jail < Safemode::Jail
+  allow :run_list
 end
